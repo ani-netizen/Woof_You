c49f7e9aee3f2b87ec1674d79b5360cd9e28c927
Hetsss2212 <hrnp2212@gmail.com>
2022-10-08 17:02:02 +0530

    startedt frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import TabGroup from "./components/TabGroup";
 
 function App() {
   return (
     <>
     <h1 className="bg-slate-400 text-9xl font-bold flex flex-col">Woof You</h1>
     <Navbar />
+    <TabGroup />
     </>
   );
 }
--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+    <div className="flex justify-between items-center px-10 py-5 w-full shadow-md sticky top-0 z-50 bg-slate-50">
+      <div className='flex gap-20'>
+      <div className="w-20 h-15 bg-amber-300">
+        logo
+      </div>
+      <ul className='flex gap-5'>
+        <li>News</li>
+        <li>Contact Us</li>
+      </ul>
+      </div>
+      <div>
+        <a href="#">Login/Signup</a>
+      </div>
+    </div>
+    </>
+    
   )
 }
 
